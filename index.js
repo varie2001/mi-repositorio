a53e92e85c87efa9a9a9a84e53a9e7d4bb443073
varie99 <varie99@yahoo.com>
2021-05-17 14:17:10 -0300

    Enviamos un mensaje con los datos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 //modulo path es para conciliar la \ de win con la / de unix
 const path = require('path');
-const ejs = require('ejs')
+const ejs = require('ejs');
 
 
 //.....settings.....
